f2f3bd31f9221e89d47822a2156b4dd3c8354bfe
Gabryan1995 <gabryan1995@gmail.com>
2020-08-04 15:57:32 -0500

    App Starting Point, Fab and Theme setup

---
+++ app/src/main/java/com/example/checklistapp/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.checklistapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        FloatingActionButton fab = findViewById(R.id.new_checklist_fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+}
