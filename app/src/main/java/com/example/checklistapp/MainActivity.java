90b6a0bb345a9a18b710c3e755240318fe52cb86
Gabryan1995 <gabryan1995@gmail.com>
2020-08-05 23:37:32 -0500

    Added Search Bar

--- app/src/main/java/com/example/checklistapp/MainActivity.java
+++ app/src/main/java/com/example/checklistapp/MainActivity.java
@@ -1,8 +1,14 @@
 package com.example.checklistapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
 
+import android.app.SearchManager;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -14,6 +20,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        handleIntent(getIntent());
 
         FloatingActionButton fab = findViewById(R.id.new_checklist_fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -23,5 +30,31 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+
+        // Associate searchable configuration with the SearchView
+        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        SearchView searchView = (SearchView) menu.findItem(R.id.action_search).getActionView();
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+
+        return true;
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+        handleIntent(intent);
+    }
+
+    private void handleIntent(Intent intent) {
+        if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
+            String query = intent.getStringExtra(SearchManager.QUERY);
+            Snackbar.make(findViewById(android.R.id.content).getRootView(), "Handle query: " + query, Snackbar.LENGTH_LONG).setAction("Action", null).show();
+        }
     }
 }
